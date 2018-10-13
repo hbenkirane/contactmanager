d4b6a39719df2f75ec66974cd1289975665c005b
Cédric Petter <cedric.petter@did-c.com>
2018-10-13 22:04:32 +0200

    Updates

---
+++ precache-manifest.7ec2a5943102ad8f1ce447866c48fb8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "16ce85867b128bfec6b8",
+    "url": "/contactmanager/static/js/main.16ce8586.chunk.js"
+  },
+  {
+    "revision": "9b4017a5a46efab20aec",
+    "url": "/contactmanager/static/js/1.9b4017a5.chunk.js"
+  },
+  {
+    "revision": "16ce85867b128bfec6b8",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "9b4017a5a46efab20aec",
+    "url": "/contactmanager/static/css/1.929ea5f7.chunk.css"
+  },
+  {
+    "revision": "3c9e2be5c26372ed973c1b3eba829d72",
+    "url": "/contactmanager/index.html"
+  }
+];
