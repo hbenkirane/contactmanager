f4763f35fbad13c3f57b1c00187772f55002ec7d
Cédric Petter <cedric.petter@did-c.com>
2018-10-13 21:57:59 +0200

    initial commit

---
+++ build/precache-manifest.e74a5bff2756787b1a850bb8fb84ebc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "544f57e5a28bcfc08a05",
+    "url": "/static/js/main.544f57e5.chunk.js"
+  },
+  {
+    "revision": "5330ed69ce815c5d8827",
+    "url": "/static/js/1.5330ed69.chunk.js"
+  },
+  {
+    "revision": "544f57e5a28bcfc08a05",
+    "url": "/static/css/main.9873f146.chunk.css"
+  },
+  {
+    "revision": "5330ed69ce815c5d8827",
+    "url": "/static/css/1.40c8ae3f.chunk.css"
+  },
+  {
+    "revision": "c967cbae12ba8fc12b8f5e1c5ea4bf98",
+    "url": "/index.html"
+  }
+];
